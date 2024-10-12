20ad449cdd7350958fc2e89cba684929574c6342
gabrielbrasiln <36045138+gabrielbrasiln@users.noreply.github.com>
2024-10-12 12:33:11 -0300

    Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'main-bg': '#020103',
+        'primary': "#FFB600",
+        'dark-yellow': "#1B1700",
+      },
+      fontFamily: {
+        'sans': ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'hero': "url('./img/bg-hero.jpg')",
+      }
+    },
+  },
+  plugins: [
+		require('tailwindcss-animated'),
+    require('tailwindcss-intersect')
+	],
+}
+
